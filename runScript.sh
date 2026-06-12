19a68e6ed286b042bcbdbe40e85da37fd525f4c0
Joshua-Kloepfer <62162034+Joshua-Kloepfer@users.noreply.github.com>
2026-06-11 20:13:24 -0400

    Refactor runScript.sh to simplify parameters and loops

--- runScript.sh
+++ runScript.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 # srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./executable
 
-minSize=$1
-maxSize=$2
-sizeIncr=$3
-minElem=$4
-maxElem=$5
-elemIncr=$6
-useCab=$7
-runsPer=$8
+minElem=$1
+maxElem=$2
+elemIncr=$3
+useCab=$4
+runsPer=$5
+order=$6
+dim=$7
 
-echo "RUNNING WITH: minSize:$minSize, maxSize:$maxSize, sizeIncr:$sizeIncr, minElem:$minElem, maxElem:$maxElem, elemIncr:$elemIncr, useCab:$useCab, runsPer:$runsPer"
+ulimit -s unlimited
 
-for ((size=minSize; size<=maxSize; size+=sizeIncr)); do
-	for ((numElem=minElem; numElem<=maxElem; numElem+=elemIncr)); do
-		srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./builds/AMD_GFX90A/meshfields/IntegratorPerformance $useCab 1 $size $numElem 2 $runsPer >> meshfields_output.txt
-		srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./builds/AMD_GFX90A/meshfields/IntegratorPerformance $useCab 1 $size $numElem 3 $runsPer >> meshfields_output.txt
-		srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./builds/AMD_GFX90A/meshfields/IntegratorPerformance $useCab 2 $size $numElem 2 $runsPer >> meshfields_output.txt
-		srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./builds/AMD_GFX90A/meshfields/IntegratorPerformance $useCab 2 $size $numElem 3 $runsPer >> meshfields_output.txt
+echo "RUNNING WITH: minElem:$minElem, maxElem:$maxElem, elemIncr:$elemIncr, useCab:$useCab, runsPer:$runsPer, order: $order, dim: $dim"
+# srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./builds/AMD_GFX90A/meshfields/IntegratorPerformance $useCab $order $minSize $minElem $dim $runsPer > tmp
 
-		srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./meshfields/mfemTest/IntegratorMfemTest --order 1 --size $size --numElem $numElem --type 2 --runs $runsPer >> mfem_output.txt
-		srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./meshfields/mfemTest/IntegratorMfemTest --order 1 --size $size --numElem $numElem --type 1 --runs $runsPer >> mfem_output.txt
-		srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./meshfields/mfemTest/IntegratorMfemTest --order 2 --size $size --numElem $numElem --type 2 --runs $runsPer >> mfem_output.txt
-		srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic ./meshfields/mfemTest/IntegratorMfemTest --order 2 --size $size --numElem $numElem --type 1 --runs $runsPer >> mfem_output.txt
-	done
+for numElem in $(awk -v min="$minElem" -v max="$maxElem" -v incr="$elemIncr" 'BEGIN {
+	for(i=min; i<=max; i = int(i * incr)) print i}'); do
+	srun -N1 -n1 -c1 --cpu-bind=threads --threads-per-core=1 -m block:cyclic rocprof --hip-trace ./builds/AMD_GFX90A/meshfields/IntegratorPerformance $useCab $order 1 $numElem $dim $runsPer
 done
